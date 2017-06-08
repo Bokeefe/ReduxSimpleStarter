59b5f605fcb7f960df5c80ab1ac68eb468544fc5
Brendan O'Keefe <brndnokf@yahoo.com>
2017-06-08 12:01:09 -0600

    Search Bar working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyCUi9IG66fxkskdHFlDaxTNBsxDw-XTSN8';
 
 const App = function(){
-  return <div>HI</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
