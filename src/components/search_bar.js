a473cd43b0dfa17c0850e99f2221feb9894b4348
Brendan O'Keefe <brndnokf@yahoo.com>
2017-06-20 14:53:27 -0600

    click function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,11 @@ class SearchBar extends Component {
 
    render(){
        return ( 
-           <div>
-            <input 
-                value={this.state.term}
-                onChange={event=>this.setState({term:event.target.value})} /> 
-                <p>{this.state.term}</p>
+           <div className="search-bar">
+                <input 
+                    value={this.state.term}
+                    onChange={event=>this.setState({term:event.target.value})} /> 
+                    <p>{this.state.term}</p>
           </div>
        );
    }
